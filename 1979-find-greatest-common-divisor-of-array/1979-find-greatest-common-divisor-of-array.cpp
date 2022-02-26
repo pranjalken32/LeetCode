06f9ec52fa7481c515484535b74717d10581e94b
Pranjal Paira <pranjal.209309016@muj.manipal.edu>
2022-02-26 10:53:08 +0530

    Time: 4 ms (92.17%), Space: 12.5 MB (16.23%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
@@ -6,7 +6,7 @@ public:
         int n=nums.size();
         a=(nums[0]);
         b=(nums[n-1]);
-        for(int i=1;i<=b;i++){
+        for(int i=2;i<=b;i++){
             if(((a%i) == 0) && ((b%i) == 0)){
                 c=i;
             
