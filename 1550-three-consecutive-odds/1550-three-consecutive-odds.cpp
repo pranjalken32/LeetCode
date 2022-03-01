fd96bb35aa51f808f0976b5b91fb758cf7b24e58
Pranjal Paira <pranjal.209309016@muj.manipal.edu>
2022-03-01 16:54:48 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (8.39%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
     bool threeConsecutiveOdds(vector<int>& arr) {
         int n=arr.size();
-        for(int i=0;i<arr.size();i++)
+        for(int i=2;i<arr.size();i++)
         {
-            if(i+2 < n)
-            {
-                if(arr[i]%2==1 && arr[i+1]%2==1 && arr[i+2]%2==1)
+                if(arr[i-1]%2==1 && arr[i]%2==1 && arr[i-2]%2==1)
                     return true;
-            }
+            
         }
         return false;
         
