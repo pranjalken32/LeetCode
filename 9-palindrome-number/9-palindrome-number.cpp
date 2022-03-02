d037126aa718592b41178098f49c729150720678
Pranjal Paira <pranjal.209309016@muj.manipal.edu>
2022-03-03 00:58:16 +0530

    Time: 8 ms (90.28%), Space: 5.8 MB (91.41%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long int temp= x;
+        long int a=0;
+       long int reverse=0;
+        if(x>=0){
+            while(x>0){
+                a=x%10;
+                reverse= reverse*10+a;
+                x=x/10;
+            } 
+            if(temp==reverse)
+                return true;
+            else
+                return false;
+        }
+        else{
+            return false;
+        }
+        
+    }
+};
