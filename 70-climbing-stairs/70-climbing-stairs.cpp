55d56e69f31e719de2133def816477c097dc7569
Pranjal Paira <pranjal.209309016@muj.manipal.edu>
2022-04-05 02:29:01 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (97.10%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        int a[100];
+    int fib(int n) {
+        if(n==0)
+            return 0;
         if(n==1)
             return 1;
-        if(n==2)
-            return 2;
-        a[0]=0;
-        a[1]=1;
-        a[2]=2;
         
-        for(int i=3;i<=n;i++)
+        int f[n+1];
+        f[0]=0;
+        f[1]=1;
+        
+        for(int i =2;i<=n;i++)
         {
-            a[i]=a[i-2]+a[i-1];
+            f[i]=f[i-2]+f[i-1];
         }
         
-        return a[n];
-        
+        return f[n];
     }
 };
