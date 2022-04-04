27a105f785987ba9bef8f85a800ae437e79e4066
Pranjal Paira <pranjal.209309016@muj.manipal.edu>
2022-04-05 02:09:26 +0530

    Time: 4 ms (52.03%), Space: 5.9 MB (79.99%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,9 +1,20 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<2)
-            return n;
-        return fib(n-2)+fib(n-1);
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
         
+        int f[n+1];
+        f[0]=0;
+        f[1]=1;
+        
+        for(int i =2;i<=n;i++)
+        {
+            f[i]=f[i-2]+f[i-1];
+        }
+        
+        return f[n];
     }
 };
