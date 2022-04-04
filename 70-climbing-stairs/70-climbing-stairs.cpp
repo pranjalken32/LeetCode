f7652702b47acee49f1dbc889e6a8c62f5f6f247
Pranjal Paira <pranjal.209309016@muj.manipal.edu>
2022-04-05 02:26:32 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (97.10%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -6,11 +6,11 @@ public:
             return 1;
         if(n==2)
             return 2;
-        a[0]=1;
+        a[0]=0;
         a[1]=1;
         a[2]=2;
         
-        for(int i=2;i<=n;i++)
+        for(int i=3;i<=n;i++)
         {
             a[i]=a[i-2]+a[i-1];
         }
