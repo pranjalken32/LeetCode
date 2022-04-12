360a9c132803dbc26ce4913216e3a9927a175def
Pranjal Paira <pranjal.209309016@muj.manipal.edu>
2022-04-13 01:02:40 +0530

    Time: 14 ms (75.36%), Space: 10.8 MB (50.83%) - LeetHub

---
+++ 1-two-sum/1-two-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+		unordered_map<int, int> hmap;
+		for (int i = 0; i < nums.size();++i) {
+			if (hmap.find(target - nums[i]) != hmap.end()) {
+				return { hmap[target - nums[i]],i };
+			}
+			hmap[nums[i]] = i;
+		}
+		return { 0,0 };
+	}
+};
