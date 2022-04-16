06a6e27223cca11b6071fb75e86363d23dcc10be
Pranjal Paira <pranjal.209309016@muj.manipal.edu>
2022-04-17 03:48:19 +0530

    Time: 292 ms (65.96%), Space: 101.8 MB (6.99%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int minPairSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
         vector<int>v;
-        for(int i=0;i<nums.size();i++)
-        {
-            v.push_back(nums[i]+nums[nums.size()-1-i]);
+        sort(nums.begin(),nums.end());
+        for(int i=0,j=nums.size()-1;i<j;i++,j--){
+            v.push_back(nums[i]+nums[j]);
         }
         int ans=*max_element(v.begin(),v.end());
         return ans;
-        
     }
 };
