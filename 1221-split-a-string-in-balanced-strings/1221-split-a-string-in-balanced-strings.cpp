d6729c41f0975bb2046e4f44234b4fb6ddc7a5cd
Pranjal Paira <pranjal.209309016@muj.manipal.edu>
2022-04-17 03:26:43 +0530

    Time: 4 ms (21.18%), Space: 6.3 MB (39.07%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int c=0,ans=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='L')
+                c++;
+            else if(s[i]=='R')
+                c--;
+            if(c==0)
+                ans++;
+        }
+        return ans;
+    }
+};
