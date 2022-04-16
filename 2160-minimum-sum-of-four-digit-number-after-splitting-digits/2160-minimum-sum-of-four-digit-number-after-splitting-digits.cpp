75648b184c3614e04f1d67015599cfb1e546cb1d
Pranjal Paira <pranjal.209309016@muj.manipal.edu>
2022-04-17 03:07:25 +0530

    Time: 0 ms (100.00%), Space: 6 MB (68.62%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.cpp
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     int minimumSum(int num) {
-        int c=num;
-        vector<int>v;
-        while(c!=0)
-        {
-            
-            v.push_back(c%10);
-            c/=10;
-        }
-        
-        sort(v.begin(),v.end());
-        int min=v[0]*10+v[3]+v[1]*10+v[2];
-        return min;
+      string s = to_string(num);
+        //converting to the string will be easier in these cases 
+        //we can see that
+        sort(s.begin(), s.end());
+        int ans=(s[0]-'0'+s[1]-'0')*10+(s[2]-'0')+(s[3]-'0');
+        return ans;
     }
 };
