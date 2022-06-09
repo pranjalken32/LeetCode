e7c52cbac4e5a8c7183972aba36a58b1a935ec1c
Pranjal Paira <pranjal.209309016@muj.manipal.edu>
2022-06-09 18:37:34 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (77.15%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int output=0;
+         while(x != 0){
+            if(output > INT_MAX/10 || output < INT_MIN/10)
+                return 0;
+            output = output*10 + x%10;
+            x /= 10;
+    }
+        
+        return output;
+    }
+};
