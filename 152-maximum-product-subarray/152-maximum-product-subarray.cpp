cd77015031fc2e99fa837ed93589e54f22378a51
Pranjal Paira <pranjal.209309016@muj.manipal.edu>
2022-07-19 02:59:45 +0530

    Time: 4 ms (93.74%), Space: 13.8 MB (72.12%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-    int n=nums.size();
-        int product=1;
-        int max_product=INT_MIN;
-        for(int i=0;i<n;i++)
+        int max=INT_MIN,currproduct=1;
+        for(int i=0;i<nums.size();i++)
         {
-            product*=nums[i];
-            if(max_product<product)
-                max_product=product;
-            if(product==0)
-                product=1;
+            currproduct*=nums[i];
+            if(max<currproduct)
+                max=currproduct;
+            if(currproduct==0)
+                currproduct=1;
         }
-        product=1;
-        for(int i=n-1;i>=0;i--)
+        currproduct=1;
+        for(int i=nums.size()-1;i>=0;i--)
         {
-            product*=nums[i];
-            max_product=max(product,max_product);
-            if(product==0)
-                product=1;
+            currproduct*=nums[i];
+            if(max<currproduct)
+                max=currproduct;
+            if(currproduct==0)
+                currproduct=1;
         }
         
-        return max_product;
+        return max;
     }
 };
