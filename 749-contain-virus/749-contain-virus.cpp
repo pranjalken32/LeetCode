43802ae28125b4a3ba4c28c519e4080925eed094
Pranjal Paira <pranjal.209309016@muj.manipal.edu>
2022-07-22 16:31:41 +0530

    Time: 38 ms (74.45%), Space: 12.8 MB (79.56%) - LeetHub

--- 749-contain-virus/749-contain-virus.cpp
+++ 749-contain-virus/749-contain-virus.cpp
@@ -1,96 +1,117 @@
-class cluster
-{
+class Group{
     public:
-        set<pair<int,int>> c; // set of cells that are contaminated
-        set<pair<int,int>> uc; // set of cells that are next to this cluster and are uncontaminated
-        int wcnt = 0;       // walls required to contain this structure 
     
+    set<pair<int,int>>ne;
+    set<pair<int,int>>affected;
+    
+    int w;
+    
+    Group(){
+        w = 0;
+    }
 };
+
+
+
 class Solution {
 public:
     
-    void dfs(vector<vector<int>> &grid, int i, int j, vector<vector<bool>> &vis, cluster &cl)
-    {
-        if(i == grid.size() || i < 0 || j < 0 || j == grid[0].size()||vis[i][j]||grid[i][j] == -1)
-        {
-            // if the current src is out of grid bounds or if the cell is already visited  or current cell is already walled that is -1
+    bool isValid(int i,int j,int n,int m){
+        return i>=0 && j>=0 && i<n && j<m;
+    }
+    
+    int dir[4][2]={
+        {1,0},
+        {0,1},
+        {-1,0},
+        {0,-1}
+    };
+    
+
+    void dfs(vector<vector<int>>&in,int i,int j,int n,int m,vector<vector<bool>>&vis,Group *g){
+        
+        if(in[i][j]==-1) return;
+        
+        if(in[i][j] ==0 ){
+            g->w++;
+            g->ne.insert({i,j});
             return;
         }
-        if(grid[i][j] == 0)
-        {
-            // if cell is uncontaminated then add to the uncontaminatd cells (uc) and increment wall cnt 
-            cl.wcnt++;
-            cl.uc.insert({i,j});
-            return;
+        
+        vis[i][j] = 1;
+        
+        if(in[i][j] == 1)
+            g->affected.insert({i,j});
+           
+        
+        for(int k=0;k<4;k++){
+            
+            int x = i+dir[k][0], y = j+dir[k][1];
+            
+            if(isValid(x,y,n,m) && !vis[x][y]) 
+                dfs(in,i+dir[k][0],j+dir[k][1],n,m,vis,g);
         }
-        // if current cell is contaminated add to contaminated and mark vis = true
-        cl.c.insert({i,j});
-        vis[i][j] = true;
-        // call dfs in all directions
-        dfs(grid,i+1,j,vis,cl); // above
-        dfs(grid,i-1,j,vis,cl); // below
-        dfs(grid,i,j+1,vis,cl); // right
-        dfs(grid,i,j-1,vis,cl); // left
         
     }
-
-    int containVirus(vector<vector<int>>& grid) {
     
-        // row ka size
-        int n = grid.size();
-        // col ka size
-        int m = grid[0].size();
-        int ans = 0;
-        while(1)
-        {
-            // visisted matrix
-            vector<vector<bool>> vis(n,vector<bool>(m));
-            // comparator for pq to sort it according to size of uncontaminated cells in cluster
-            auto comp=[](const cluster& A,const cluster& B)
-            { 
-                return A.uc.size()<B.uc.size();
-            };
-            // initialise priority queue
-            priority_queue<cluster,vector<cluster>, decltype(comp)> pq(comp);
-            // search for a contaminated cell in the grid
-            for(int i = 0; i<n; i++)
-            {
-                for(int j = 0; j<m; j++)
-                {
-                    if(!vis[i][j]&&grid[i][j] == 1)
-                    {
-                        // create a cluster
-                        cluster c;
-                        // call dfs
-                        dfs(grid,i,j,vis,c);
-                        pq.push(c);
-
+    
+    int containVirus(vector<vector<int>>& in) {
+        
+        int n = in.size(),m = in[0].size(),ans = 0;
+        
+        
+        while(1){
+            
+            vector<Group *>v;
+            
+            vector<vector<bool>>visited(n,vector<bool>(m,0));
+            
+            for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    if(!visited[i][j] && in[i][j]==1){
+                        
+                        Group *g  = new Group();
+                        dfs(in,i,j,n,m,visited,g);
+                        
+                        v.push_back(g);
                     }
+                        
                 }
             }
-            // if size of priority queue is 0 the termination condition is reached ie all contaminated cells are contained.
-            if(pq.size()==0)
-            break;
-            // get the biggest cluster is found make em all -1 and add the walls reqd to contain them 
-            cluster k = pq.top();
-            pq.pop();
-            for(auto x: k.c)
-            {
-                grid[x.first][x.second] = -1;
+            
+            
+            if(v.size()==0) break;
+            
+            
+            int mx = 0,index = 0;
+            
+            for(int i=0;i<v.size();i++){
+                if(v[i]->ne.size()>mx){
+                    mx = v[i]->ne.size();
+                    index = i;
+                }
             }
-            ans += k.wcnt;
-
-            // for the rest of the clusters make their uncontaminated cells as contaminated
-            while(!pq.empty())
-            {
-                cluster k1 = pq.top();
-                pq.pop();
-                for(auto x: k1.uc)
-                {
-                    grid[x.first][x.second] = 1;
-                }   
+            
+            Group * x = v[index];
+            
+            ans+=(x->w);
+            
+            
+            for(auto itr:x->affected){
+                in[itr.first][itr.second] = -1;
             }
-         
+            
+            
+            for(int i=0;i<v.size();i++){
+                if(i==index) continue;
+                x = v[i];
+                for(auto itr:x->ne){
+                    in[itr.first][itr.second] = 1;
+                }
+                
+            }
+            
+ 
         }
         
         return ans;
