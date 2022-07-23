3e3e8d3db3cdfa47c755f0163b32e2b0666e5699
Pranjal Paira <pranjal.209309016@muj.manipal.edu>
2022-07-24 00:58:04 +0530

    Time: 189 ms (94.04%), Space: 10.2 MB (69.44%) - LeetHub

---
+++ 204-count-primes/204-count-primes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+	int countPrimes(int n) {
+	
+		vector<bool> prime(n + 1, true);
+		for (int i = 2; i * i <= n; i++) {
+			if (prime[i]) {
+				for (int j = i * i; j <= n; j += i) {
+					prime[j] = false;
+				}
+			}
+		}
+		int primeCount = 0;
+		for (int i = 2; i < n; i++) {
+			if (prime[i]) {
+				primeCount++;
+			}
+		}
+		return primeCount;
+	}
+};
