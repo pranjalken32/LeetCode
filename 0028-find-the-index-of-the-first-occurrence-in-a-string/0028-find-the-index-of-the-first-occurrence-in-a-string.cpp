e2e5a7b2422c206e547f123c4f5c0466b0fc9f58
Pranjal Paira <pranjal.209309016@muj.manipal.edu>
2022-10-13 18:05:09 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (26.67%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -4,11 +4,10 @@ public:
         int l=needle.length();
         int flag=0;
         int m=haystack.length();
-        string x="";
+        
         for(int i=0;i<m;i++)
         {
-            x=haystack.substr(i,l);
-            if(x==needle)
+            if(haystack.substr(i,l)==needle)
                 return i;
         }
         return -1;
